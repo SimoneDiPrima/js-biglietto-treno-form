ab4b61f7f83f2bd80b02e2b6cc555af562e59194
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-23 09:26:32 +0200

    impostazione layout

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const nameInput = document.getElementById("Name-input");
+console.log(nameInput);
+const distanceInput = document.getElementById("distance-input");
+console.log(distanceInput);
+const ageInput = document.getElementById("age-input");
+console.log(ageInput);
