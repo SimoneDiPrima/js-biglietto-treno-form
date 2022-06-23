46cc9abbd99bba7112da80ce63bb408dbd8f776f
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-23 18:39:45 +0200

    impostazione card biglietto

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-const nameInput = document.getElementById("Name-input");
-console.log(nameInput);
-const distanceInput = document.getElementById("distance-input");
-console.log(distanceInput);
-const ageInput = document.getElementById("age-input");
-console.log(ageInput);
+const nameValue = document.getElementById("Name-input");
+console.log(nameValue);
+const distanceValue = document.getElementById("distance-input");
+console.log(distanceValue);
+const ageValue = document.getElementById("age-input");
+console.log(ageValue);
+
+const confirmButton = document.getElementById(`confirm-button`);
+const resetButton = document.getElementById(`reset-button`);
+
