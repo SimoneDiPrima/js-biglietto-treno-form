7b80e05ed68bbcf11e0ccdbf3acbb523c72dc3fb
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-24 14:42:51 +0200

    creazioni bottoni

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-const nameValue = document.getElementById("Name-input");
-console.log(nameValue);
-const distanceValue = document.getElementById("distance-input");
-console.log(distanceValue);
-const ageValue = document.getElementById("age-input");
-console.log(ageValue);
+const nameInput = document.getElementById("Name-input");
+console.log(nameInput);
+const distanceInput = document.getElementById("distance-input");
+console.log(distanceInput);
+const ageInput = document.getElementById("age-input");
+console.log(ageInput);
 
 const confirmButton = document.getElementById(`confirm-button`);
 const resetButton = document.getElementById(`reset-button`);
 
+confirmButton.addEventListener(`click`, function(){
+    const nameValue = nameInput.value;
+    const distanceValue = distanceInput.value;
+    const ageValue = ageInput.value;
+    console.log(nameValue);
+    console.log(distanceValue);
+    console.log(ageValue);
+    
+}
+)
