76b2ef5bbabfe0538f9601dbd8faa744deaa93cb
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-26 17:53:55 +0200

    esercizio completo form base

--- js/script.js
+++ js/script.js
@@ -1,20 +1,65 @@
-const nameInput = document.getElementById("Name-input");
+const nameInput = document.getElementById("Nameinput");
 console.log(nameInput);
-const distanceInput = document.getElementById("distance-input");
+const distanceInput = document.getElementById("distanceinput");
 console.log(distanceInput);
-const ageInput = document.getElementById("age-input");
+const ageInput = document.getElementById("eta");
 console.log(ageInput);
 
 const confirmButton = document.getElementById(`confirm-button`);
+
 const resetButton = document.getElementById(`reset-button`);
 
-confirmButton.addEventListener(`click`, function(){
-    const nameValue = nameInput.value;
-    const distanceValue = distanceInput.value;
-    const ageValue = ageInput.value;
-    console.log(nameValue);
-    console.log(distanceValue);
-    console.log(ageValue);
-    
+let namePax = document.getElementById(`nomePax`);
+let car = document.getElementById(`car`);
+let pnrReservation = document.getElementById(`reservation`);
+let totalPrice = document.getElementById(`final-price`);
+
+confirmButton.addEventListener(`click`,function(){
+   nameInput.value = nameInput.value;
+   console.log(nameInput.value);
+   distanceInput.value = distanceInput.value;
+   console.log(distanceInput.value);
+   ageInput.value = eta.value;
+   console.log(ageInput.value);
+
+   let price = distanceInput.value * 0.21;
+   console.log(price)
+   if(ageInput.value === `minorenni`){
+    price = (price/100) * 80;
+    console.log(price);
+    namePax.innerText = nameInput.value;
+    let numeroCar = Math.round(Math.random()*20-1);
+    console.log(numeroCar);
+    car.innerText = numeroCar;
+    let numeroPnr = Math.round(Math.random()*1000-1);
+    console.log(numeroPnr);
+    pnrReservation.innerText = numeroPnr;
+    totalPrice.innerText = price.toFixed(2);
+
+   }
+  else if(ageInput.value === `over65`){
+    price = ( price /100)*60;
+    console.log(price);
+    console.log(price);
+    namePax.innerText = nameInput.value;
+    let numeroCar = Math.round(Math.random()*20-1);
+    console.log(numeroCar);
+    car.innerText = numeroCar;
+    let numeroPnr = Math.round(Math.random()*1000-1);
+    console.log(numeroPnr);
+    pnrReservation.innerText = numeroPnr;
+    totalPrice.innerText = price.toFixed(2);
+  
+}
+else{
+    namePax.innerText = nameInput.value;
+    let numeroCar = Math.round(Math.random()*20-1);
+    console.log(numeroCar);
+    car.innerText = numeroCar;
+    let numeroPnr = Math.round(Math.random()*1000-1);
+    console.log(numeroPnr);
+    pnrReservation.innerText = numeroPnr;
+    totalPrice.innerText = price.toFixed(2);
+}
 }
 )
