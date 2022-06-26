1ec7e563f87c543f82ed3b85e90397eb0b321b44
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-26 19:55:31 +0200

    esercizio completo

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ const ageInput = document.getElementById("eta");
 console.log(ageInput);
 
 const confirmButton = document.getElementById(`confirm-button`);
-
 const resetButton = document.getElementById(`reset-button`);
+const finalButton = document.getElementById(`last-button`);
 
 let namePax = document.getElementById(`nomePax`);
 let discount = document.getElementById(`tariffa`);
@@ -30,12 +30,12 @@ confirmButton.addEventListener(`click`,function(){
     console.log(price);
     namePax.innerText = nameInput.value;
     discount.innerText = ageInput.value;
-    let numeroCar = Math.round(Math.random()*20-1);
+    let numeroCar = Math.round(Math.random()*20);
     console.log(numeroCar);
     car.innerText = numeroCar;
-    let numeroPnr = Math.round(Math.random()*1000-1);
+    let numeroPnr = Math.round(Math.random()*1000);
     console.log(numeroPnr);
-    pnrReservation.innerText = numeroPnr;
+    pnrReservation.innerText = `L`+ numeroPnr +`P`;
     totalPrice.innerText = price.toFixed(2);
 
    }
@@ -44,24 +44,38 @@ confirmButton.addEventListener(`click`,function(){
     console.log(price);
     console.log(price);
     namePax.innerText = nameInput.value;
-    let numeroCar = Math.round(Math.random()*20-1);
+    discount.innerText = ageInput.value;
+    let numeroCar = Math.round(Math.random()*20);
     console.log(numeroCar);
     car.innerText = numeroCar;
-    let numeroPnr = Math.round(Math.random()*1000-1);
+    let numeroPnr = Math.round(Math.random()*1000);
     console.log(numeroPnr);
-    pnrReservation.innerText = numeroPnr;
+    pnrReservation.innerText = `V`+ numeroPnr +`N`;
     totalPrice.innerText = price.toFixed(2);
   
 }
 else{
     namePax.innerText = nameInput.value;
-    let numeroCar = Math.round(Math.random()*20-1);
-    console.log(numeroCar);
+    discount.innerText = ageInput.value;
+    let numeroCar = Math.round(Math.random()*20);
+    console.log(numeroCar)
     car.innerText = numeroCar;
-    let numeroPnr = Math.round(Math.random()*1000-1);
+    let numeroPnr = Math.round(Math.random()*1000);
     console.log(numeroPnr);
-    pnrReservation.innerText = numeroPnr;
+    pnrReservation.innerText = `F`+ numeroPnr +`G`;
     totalPrice.innerText = price.toFixed(2);
 }
 }
 )
+resetButton.addEventListener(`click`,function(){
+    nameInput.value = ``;
+   console.log(nameInput.value);
+   distanceInput.value = ``;
+   console.log(distanceInput.value);
+   ageInput.value = ``;
+   console.log(ageInput.value);
+
+})
+finalButton.addEventListener(`click`,function(){
+    window.alert("COMPLIMENTI HAI COMPRATO IL BIGLIETTO!!");
+})
